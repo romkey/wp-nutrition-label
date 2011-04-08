9d15bf655ff0f80c37a1a2f93a79de8445d86d5c
John Romkey <romkey@romkey.com>
2011-04-07 21:30:04 -0400

    first cut at internationalization support

--- wp-nutrition-label.php
+++ wp-nutrition-label.php
@@ -8,6 +8,9 @@ Version: 0.1
 Author URI: http://romkey.com/
 */
 
+// if(!load_plugin_textdomain('your-unique-name','/wp-content/languages/'))
+//	load_plugin_textdomain('your-unique-name','/wp-content/plugins/plugin-name/location-of-mo-po-files/');
+
 /* add_action('widgets_init', create_function('', 'return register_widget("NutriLabelWidget");')); */
 add_shortcode('nutri-label', 'nutr_label_shortcode');
 add_action('wp_head', 'nutr_style');
@@ -100,46 +103,46 @@ function nutr_label_generate($args) {
   }
 
   return "<div ".($nutri_id ? "id='".$nutri_id."'" : "") . ($style ? $style : "") . "class='wp-nutrition-label" . ( $nutri_cssclass ? " ".$nutri_cssclass : "") . "'>
-  <h2>Nutrition Facts</h2>
-  <span class='alignleft'>Serving Size ".$nutr_servingsize."</span>
-  <span class='alignright'>Servings ".$nutr_servings."</span>
+  <h2>".__("Nutrition Facts")."</h2>
+  <span class='alignleft'>".__("Serving Size")." ".$nutr_servingsize."</span>
+  <span class='alignright'>".__("Servings")." ".$nutr_servings."</span>
   <hr class='heavy' />
   <strong>Amount Per Serving</strong>
    <hr />
-   <span class='alignleft'>Calories ".$nutr_calories."</span>
+   <span class='alignleft'>".__("Calories")." ".$nutr_calories."</span>
    <span class='alignright'>Calories from Fat ".($nutr_totalfat * 9)."</span>
    <hr />
-   <div class='alignright'><strong>% Daily Value*</strong></div><div style='clear: both'></div>
-   <span class='alignleft'><strong>Total Fat</strong> ".$nutr_totalfat."g</span>
+   <div class='alignright'><strong>% ".__("Daily Value")."*</strong></div><div style='clear: both'></div>
+   <span class='alignleft'><strong>".__("Total Fat")."</strong> ".$nutr_totalfat."g</span>
    <span class='alignright'>".nutr_percentage($nutr_totalfat, $rda['totalfat'])."%</span>
    <hr />
-   <span class='alignleft indent'>Saturated Fat ".$nutr_satfat."g</span>
+   <span class='alignleft indent'>".__("Saturated Fat")." ".$nutr_satfat."g</span>
    <span class='alignright'>".nutr_percentage($nutr_satfat, $rda['satfat'])."%</span>
    <hr />
-   <span class='indent'>Trans Fat ".$nutr_transfat."g</span>
+   <span class='indent'>".__("Trans Fat")." ".$nutr_transfat."g</span>
    <hr />
-   <span class='alignleft'><strong>Cholesterol</strong> ".$nutr_cholesterol."mg</span>
+   <span class='alignleft'><strong>".__("Cholesterol")."</strong> ".$nutr_cholesterol."mg</span>
    <span class='alignright'>".nutr_percentage($nutr_cholesterol, $rda['cholesterol'])."%</span>
    <hr />
-   <span class='alignleft'><strong>Sodium</strong> ".$nutr_sodium."mg</span>
+   <span class='alignleft'><strong>".__("Sodium")."</strong> ".$nutr_sodium."mg</span>
    <span class='alignright'>".nutr_percentage($nutr_sodium, $rda['sodium'])."%</span>
    <hr />
-   <span class='alignleft'><strong>Total Carbohydrate</strong> ".$nutr_carbohydrates."g</span>
+   <span class='alignleft'><strong>".__("Total Carbohydrate")."</strong> ".$nutr_carbohydrates."g</span>
    <span class='alignright'>".nutr_percentage($nutr_carbohydrates, $rda['carbohydrates'])."%</span>
    <hr />
-   <span class='alignleft indent'>Dietary Fiber ".$nutr_fiber."g</span>
+   <span class='alignleft indent'>".__("Dietary Fiber")." ".$nutr_fiber."g</span>
    <span class='alignright'>".nutr_percentage($nutr_fiber, $rda['fiber'])."%</span>
    <hr />
-    <span class='indent'>Sugars ".$nutr_sugars."g</span>
+    <span class='indent'>".__("Sugars")." ".$nutr_sugars."g</span>
    <hr />
-   <span class='alignleft'><strong>Protein</strong> ".$nutr_protein."g</span>
+   <span class='alignleft'><strong>".__("Protein")."</strong> ".$nutr_protein."g</span>
    <span class='alignright'>".nutr_percentage($nutr_protein, $rda['protein'])."%</span>
    <hr class='heavy' />
    Vitamin A XX% &bullet; Vitamin C XX%
    <hr />
    Calcium XX% &bullet; Iron XX%
    <hr />
-   <span class='small'>* Percent Daily Values are based on a 2,000 calorie diet. Your daily values may be higher or lower depending on your calorie needs.</span>
+   <span class='small'>* ".__("Percent Daily Values are based on a 2,000 calorie diet. Your daily values may be higher or lower depending on your calorie needs.")."</span>
    <span class='small alignright'><a href='http://www.romkey.com/code/wp-nutrition-label'>wp-nutrition-label</a>
 </div>";
 } ?>

8e178e58bdde45dbedbfe395c0d40a17522dede1
John Romkey <romkey@romkey.com>
2011-04-12 22:11:09 -0400

    Fat Calories -> Calories from Fat change h3 to span

--- wp-nutrition-label.php
+++ wp-nutrition-label.php
@@ -34,11 +34,11 @@ function nutr_style() {
   .wp-nutrition-label hr.heavy {
     height: .8em;
   }
-  .wp-nutrition-label h3 {
+  .wp-nutrition-label {
     font-size: 3em;
     font-weight: 900;
-    margin: 0px !important;
-    line-height: 1em !important;
+    margin: 0px;
+    line-height: 1em;
     text-align: center;
   }
   .wp-nutrition-label span.indent {
@@ -103,7 +103,7 @@ function nutr_label_generate($args) {
   }
 
   return "<div ".($nutri_id ? "id='".$nutri_id."'" : "") . ($style ? $style : "") . "class='wp-nutrition-label" . ( $nutri_cssclass ? " ".$nutri_cssclass : "") . "'>
-  <h2>".__("Nutrition Facts")."</h2>
+  <span>".__("Nutrition Facts")."</span>
   <span class='alignleft'>".__("Serving Size")." ".$nutr_servingsize."</span>
   <span class='alignright'>".__("Servings")." ".$nutr_servings."</span>
   <hr class='heavy' />
